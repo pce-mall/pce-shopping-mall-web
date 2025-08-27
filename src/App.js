5f23eb748525f532bb0b34c59a9d3c116185d14d
pce-mall <pceshoppingmall@gmail.com>
2025-08-27 13:49:20 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>PCE Shopping Mall</h1>
+      <p>Welcome! React is working 🎉</p>
+    </div>
+  );
+}
